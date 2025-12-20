cba8742de1495c007befb93d684ef2a28fa28221
ctkcoding <ckateyiannis@gmail.com>
2025-12-19 18:24:31 -0600

    extract all fields stripped

--- divvy.py
+++ divvy.py
@@ -1,33 +1,17 @@
 import sys
 import csv
-import html_to_json
-import json
 from bs4 import BeautifulSoup
 
 html_file = sys.argv[1]
 csv_file = "outputs/{html_file}.csv"
 
-# with open(html_file, 'r', encoding='utf-8') as f:
-#     html_string = f.read()
-
-# TODO - initial parsing of entire file
-# output_json_dict = html_to_json.convert(html_string)
-# json_string = json.dumps(output_json_dict, indent=4)
-
-# print(json_string)
-
-
-
 # TODO - every field i want
 # price? basically useless, most are free
 # duration? compute from start and end time
-
 # date      sc-cx1xxi-0 eOzbdu
-
 # start     Components__Start-sc-1yewqdd-0 bSShsJ
 # .station  sc-cx1xxi-0 bRSchk
 # .time     Components__TextMuted-sc-19oq86k-0 bepYxb
-
 # end       Components__End-sc-1yewqdd-1 dUHFFN
 # .station  sc-cx1xxi-0 bRSchk
 # .time     Components__TextMuted-sc-19oq86k-0 bepYxb
@@ -35,7 +19,6 @@ csv_file = "outputs/{html_file}.csv"
 with open(html_file, 'r', encoding='utf-8') as f:
     soup = BeautifulSoup(f, 'html.parser')
 
-# Find the specific table you want to extract (adjust selectors as needed)
 divvy_trips = soup.select('div[class*="Components__Wrapper"]')
 
 print(f"Data elements count:'{len(divvy_trips)}'")
@@ -48,7 +31,8 @@ for trip in divvy_trips:
 
     # NON NESTED
     # date      sc-cx1xxi-0 eOzbdu
-    date = trip.find('h2', class_='sc-cx1xxi-0 eOzbdu').text
+    date_list = trip.find('h2', class_='sc-cx1xxi-0 eOzbdu').get_text().splitlines()
+    date = date_list[0].strip() + " " + date_list[1].strip()
     print(date)
 
     # TODO - skip and mark if no start/end
@@ -57,26 +41,23 @@ for trip in divvy_trips:
     # start     Components__Start-sc-1yewqdd-0 bSShsJ
     start = trip.find('div', class_='Components__Start-sc-1yewqdd-0 bSShsJ')
     # .station  sc-cx1xxi-0 bRSchk
-    start_station   = start.find('div', class_='sc-cx1xxi-0 bRSchk').text
+    start_station = start.find('div', class_='sc-cx1xxi-0 bRSchk').get_text().splitlines()[0]
     print(start_station)
     # .time     Components__TextMuted-sc-19oq86k-0 bepYxb
-    start_time      = start.find('div', class_='Components__TextMuted-sc-19oq86k-0 bepYxb').text
+    start_time = start.find('div', class_='Components__TextMuted-sc-19oq86k-0 bepYxb').get_text()
     print(start_time)
 
 
     # end       Components__End-sc-1yewqdd-1 dUHFFN
     end = trip.find('div', class_='Components__End-sc-1yewqdd-1 dUHFFN')
     # .station  sc-cx1xxi-0 bRSchk
-    end_station = trip.find('div', class_='sc-cx1xxi-0 bRSchk').text
+    end_station = end.find('div', class_='sc-cx1xxi-0 bRSchk').get_text().splitlines()[0]
     print(end_station)
+
     # .time     Components__TextMuted-sc-19oq86k-0 bepYxb
-    end_time = trip.find('div', class_='Components__TextMuted-sc-19oq86k-0 bepYxb').text
+    end_time = end.find('div', class_='Components__TextMuted-sc-19oq86k-0 bepYxb').get_text()
     print(end_time)
 
-    # output_json_dict = html_to_json.convert(trip.get_text)
-    # json_string = json.dumps(output_json_dict, indent=4)
-    # print(json_string)
-    # print(f"Data:'{trip.get_text}'")
 
 # # Open the CSV file for writing
 # with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
