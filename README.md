8398cd699b597730edde7cd23af6f1502a937ceb
ctkcoding <ckateyiannis@gmail.com>
2025-12-19 19:09:24 -0600

    write to csv file with headers

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # divvy-history-parser
 parsing my divvy history to visualize my trips
+
+
+# usage
+takes an html file of data stripped from the divvy website, just the <div class="Components__Wrapper-sc-1ff1hfy-0 kHAWET"> and nothing higher in the chain
--- divvy.py
+++ divvy.py
@@ -3,7 +3,7 @@ import csv
 from bs4 import BeautifulSoup
 
 html_file = sys.argv[1]
-csv_file = "outputs/{html_file}.csv"
+csv_file = "output.csv"
 
 # TODO - every field i want
 # price? basically useless, most are free
@@ -19,58 +19,68 @@ csv_file = "outputs/{html_file}.csv"
 with open(html_file, 'r', encoding='utf-8') as f:
     soup = BeautifulSoup(f, 'html.parser')
 
-divvy_trips = soup.select('div[class*="Components__Wrapper"]')
+headers = ['Date', 'Start station', 'Start time', 'End station', 'End time']
+# Open the CSV file for writing
+with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    # Extract headers
+    writer.writerow(headers)
 
-print(f"Data elements count:'{len(divvy_trips)}'")
+    divvy_trips = soup.select('div[class*="Components__Wrapper"]')
 
-if not divvy_trips:
-    print(f"Fields not found in {html_file}")
-    sys.exit(1)
+    print(f"Data elements count:'{len(divvy_trips)}'")
 
-for trip in divvy_trips:
+    if not divvy_trips:
+        print(f"Fields not found in {html_file}")
+        sys.exit(1)
 
-    # NON NESTED
-    # date      sc-cx1xxi-0 eOzbdu
-    date_list = trip.find('h2', class_='sc-cx1xxi-0 eOzbdu').get_text().splitlines()
-    date = date_list[0].strip() + " " + date_list[1].strip()
-    print(date)
+    for trip in divvy_trips:
 
-    # TODO - skip and mark if no start/end
+        # NON NESTED
+        # date      sc-cx1xxi-0 eOzbdu
+        date_list = trip.find('h2', class_='sc-cx1xxi-0 eOzbdu').get_text().splitlines()
+        date = date_list[0].strip()
+        if len(date_list) > 1:
+            date += " " + date_list[1].strip()
+        print(date)
 
-    # NESTED
-    # start     Components__Start-sc-1yewqdd-0 bSShsJ
-    start = trip.find('div', class_='Components__Start-sc-1yewqdd-0 bSShsJ')
-    # .station  sc-cx1xxi-0 bRSchk
-    start_station = start.find('div', class_='sc-cx1xxi-0 bRSchk').get_text().splitlines()[0]
-    print(start_station)
-    # .time     Components__TextMuted-sc-19oq86k-0 bepYxb
-    start_time = start.find('div', class_='Components__TextMuted-sc-19oq86k-0 bepYxb').get_text()
-    print(start_time)
+        start_station = ""
+        start_time = ""
+        end_station = ""
+        end_time = ""
+
+        # NESTED
+        # start     Components__Start-sc-1yewqdd-0 bSShsJ
+        start = trip.find('div', class_='Components__Start-sc-1yewqdd-0 bSShsJ')
+        
+        # TODO - skip and mark if no start/end
+        if start:
+            # .station  sc-cx1xxi-0 bRSchk
+            start_station = start.find('div', class_='sc-cx1xxi-0 bRSchk').get_text().splitlines()[0]
+            print(start_station)
+            # .time     Components__TextMuted-sc-19oq86k-0 bepYxb
+            start_time = start.find('div', class_='Components__TextMuted-sc-19oq86k-0 bepYxb').get_text()
+            print(start_time)
 
 
-    # end       Components__End-sc-1yewqdd-1 dUHFFN
-    end = trip.find('div', class_='Components__End-sc-1yewqdd-1 dUHFFN')
-    # .station  sc-cx1xxi-0 bRSchk
-    end_station = end.find('div', class_='sc-cx1xxi-0 bRSchk').get_text().splitlines()[0]
-    print(end_station)
+            # end       Components__End-sc-1yewqdd-1 dUHFFN
+            end = trip.find('div', class_='Components__End-sc-1yewqdd-1 dUHFFN')
+            if end:
+                # .station  sc-cx1xxi-0 bRSchk
+                end_station = end.find('div', class_='sc-cx1xxi-0 bRSchk').get_text().splitlines()[0]
+                print(end_station)
 
-    # .time     Components__TextMuted-sc-19oq86k-0 bepYxb
-    end_time = end.find('div', class_='Components__TextMuted-sc-19oq86k-0 bepYxb').get_text()
-    print(end_time)
+                # .time     Components__TextMuted-sc-19oq86k-0 bepYxb
+                end_time = end.find('div', class_='Components__TextMuted-sc-19oq86k-0 bepYxb').get_text()
+                print(end_time)
 
+        if not start:
+            print(f"Start/End not found for {date}. Skipping")
 
-# # Open the CSV file for writing
-# with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
-#     writer = csv.writer(csvfile)
-    
-#     # Extract headers
-#     headers = [header.get_text(strip=True) for header in table.find_all('th')]
-#     writer.writerow(headers)
-    
-#     # Extract data rows
-#     for row in table.find_all('tr')[1:]: # Skip the header row
-#         csv_row = [cell.get_text(strip=True) for cell in row.find_all('td')]
-#         if csv_row: # Ensure the row is not empty
-#             writer.writerow(csv_row)
+        data_row = [date, start_station, start_time, end_station, end_time]
+        print(data_row)
+
+        writer.writerow(data_row)
+
 
 print(f"Successfully run")
